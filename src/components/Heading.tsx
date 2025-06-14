cacfefbc1125ee728f2e24f3ce8d6aa1cf80de00
Dawsxn <dawscatignas008@gmail.com>
2025-06-14 17:06:49 +0800

    feat: create Heading component for text

---
+++ src/components/Heading.tsx
@@ -0,0 +1,32 @@
+import { JSX } from "react";
+
+type HeadingProps = {
+  children: React.ReactNode;
+  level?: 1 | 2 | 3;
+  size?:  "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
+  className?: string;
+};
+
+export function Heading({ children, level = 1, size = "lg", className = "" }: HeadingProps) {
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+
+const sizeClasses = {
+  xs: "text-[10px] sm:text-xs md:text-sm lg:text-base",
+  sm: "text-sm sm:text-base md:text-lg lg:text-xl",
+  md: "text-base sm:text-lg md:text-xl lg:text-2xl",
+  lg: "text-lg sm:text-xl md:text-2xl lg:text-3xl",
+  xl: "text-xl sm:text-2xl md:text-3xl lg:text-4xl",
+  "2xl": "text-2xl sm:text-3xl md:text-4xl lg:text-5xl",
+  "3xl": "text-3xl sm:text-4xl md:text-5xl lg:text-6xl",
+};
+
+
+
+  return (
+    <Tag
+      className={`${sizeClasses[size]} font-bold font-Poppins text-white ${className}`}
+    >
+      {children}
+    </Tag>
+  );
+}
