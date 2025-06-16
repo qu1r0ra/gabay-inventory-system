8efa24919b39478bfe04f4fca1b35c3d90ae5a8c
Dawsxn <dawscatignas008@gmail.com>
2025-06-16 20:36:31 +0800

    fix: adjusted right-side elements of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,29 @@
-// Header.jsx
-
 import Input from "./Input";
 import Button from "./Button";
 import logo from '../assets/Logo.png';
 
 function Header() {
   return (
-    <>
-      <div className="h-[100px] bg-white sticky top-0 w-full z-10 flex flex-row items-center px-6">
-        <Input
-          label=""
-          id="search-input"
-          name="searchQuery"
-          type="search"
-          placeholder="Search..."
-          className="flex-grow max-w-lg"  
-          inputClassName="rounded-2xl"   
-        />
-        <Button size="xs" className="ml-64 mb-3">
+    <div className="h-[100px] bg-white sticky top-0 w-full z-10 flex items-center px-6 border-b border-black">
+      <Input
+        label=""
+        id="search-input"
+        name="searchQuery"
+        type="search"
+        placeholder="Search..."
+        className="flex-grow max-w-lg"
+        inputClassName="rounded-2xl"
+      />
+
+      <div className="flex items-center gap-4 ml-auto">
+        <Button size="xs">
           LOGOUT
         </Button>
-        <div className="w-[2px] h-1/2 bg-gray-400 ml-6 mb-3"></div>
-        <img src={logo} className="rounded-full w-10 h-10 ml-4 mb-3"></img>
-        <h2 className="font-Poppins font-bold text-md ml-4 mb-3"> Stephen Co</h2>
+        <div className="w-[2px] h-8 bg-gray-400" />
+        <img src={logo} className="rounded-full w-10 h-10" alt="Profile" />
+        <h2 className="font-Poppins font-bold text-md">Stephen Co</h2>
       </div>
-    </>
+    </div>
   );
 }
 
