ce8f18e33ecaf12b1ac0cb2efaf0f1f6fd5cbaa4
neue.dev <modavid.1964@gmail.com>
2025-06-17 02:36:28 +0800

    feat: Implement auth hook

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import "./Layout.css";
 import NavBar from "./components/SideNavBar";
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
 import Input from "./Input";
 import Button from "./Button";
-import logo from '../assets/Logo.png';
+import logo from "../assets/Logo.png";
+import { useAuth } from "../lib/db/db.auth";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  };
+
   return (
     <div className="h-[100px] bg-white sticky top-0 w-full z-10 flex items-center px-6 border-b border-black">
       <Input
@@ -16,12 +26,12 @@ function Header() {
       />
 
       <div className="flex items-center gap-4 ml-auto">
-        <Button size="xs">
+        <Button size="xs" onClick={handleLogout}>
           LOGOUT
         </Button>
         <div className="w-[2px] h-8 bg-gray-400" />
         <img src={logo} className="rounded-full w-10 h-10" alt="Profile" />
-        <h2 className="font-Poppins font-bold text-md">Stephen Co</h2>
+        <h2 className="font-Poppins font-bold text-md">{user?.email}</h2>
       </div>
     </div>
   );
