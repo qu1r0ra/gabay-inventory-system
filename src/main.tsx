a3d293ba0b12accedc3e5ad48278b8691569fffc
neue.dev <modavid.1964@gmail.com>
2025-06-17 02:36:28 +0800

    feat: Implement auth hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import RootRouter from "./RootRouter";
 import Dashboard from "./pages/Dashboard";
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,67 +1,82 @@
-import logo from '../assets/Logo.png';
-import Input from '../components/Input';
-import Button from '../components/Button';
-import React, { useState } from 'react';
+import logo from "../assets/Logo.png";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../lib/db/db.auth";
+import { useNavigate } from "react-router-dom";
 
-function Login({ onLoginSuccess }: { onLoginSuccess: Function }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+function Login() {
+  const { user, login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin: React.FormEventHandler<HTMLFormElement> = (event) => {
-      event.preventDefault();
+  // If already authentticated, go to dashboard
+  useEffect(() => {
+    if (user) navigate("/dashboard");
+  }, [user]);
 
-      console.log('Attempting login with:', username, password);
+  const handleLogin: React.FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
 
-      if (username.trim() !== '' && password.trim() !== '') {
-        console.log('Login successful (debug mode)! Session set to true.');
-        onLoginSuccess();
-      } else {
-        console.log('Login failed: Please enter both username and password.');
-        alert('Please enter both username and password to log in.');
-      }
-    };
+    // Fields are missing
+    if (email.trim() === "" || password.trim() === "") {
+      console.log("Login failed: Please enter both username and password.");
+      alert("Please enter both username and password to log in.");
+      return;
+    }
 
-    return (
-        <div className="flex w-screen min-h-screen overflow-hidden">
-            <div className="w-1/2 bg-white flex flex-col items-center justify-start"> 
-                <img src={logo} className="w-[200px] h-[200px] mt-12"></img>
-                <h1 className="font-Poppins text-secondary font-bold mt-2 text-3xl">
-                    GABAY
-                </h1>
+    // Validate creds
+    const success = await login(email, password);
+    if (!success) return;
+  };
 
-                <form onSubmit={handleLogin} className="mt-8 w-[75%] px-4"> 
-                    <Input
-                        label="Username"
-                        id="username"
-                        name="username"
-                        type="text"
-                        placeholder="Enter your username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
+  return (
+    <div className="flex w-screen min-h-screen overflow-hidden">
+      <div className="w-1/2 bg-white flex flex-col items-center justify-start">
+        <img src={logo} className="w-[200px] h-[200px] mt-12"></img>
+        <h1 className="font-Poppins text-secondary font-bold mt-2 text-3xl">
+          GABAY
+        </h1>
 
-                    <Input
-                        label="Password"
-                        id="password"
-                        name="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+        <form onSubmit={handleLogin} className="mt-8 w-[75%] px-4">
+          <Input
+            label="Username"
+            id="username"
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-                    <Button size="md" type="submit" className="mt-8 mx-auto block"> 
-                        LOGIN
-                    </Button>
-                </form>
+          <Input
+            label="Password"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                <p className='font-Work-Sans mt-2'>Don’t have an account? <span className='text-primary underline'>Sign up.</span></p>
-            </div>
-            <div className="w-1/2 bg-primary flex items-center justify-center">
-                Right Half Content (e.g., Branding or Image)
-            </div>
-        </div>
-    );
+          <Button size="md" type="submit" className="mt-8 mx-auto block">
+            LOGIN
+          </Button>
+        </form>
+
+        <p className="font-Work-Sans mt-2">
+          Don’t have an account?{" "}
+          <span className="text-primary underline">Sign up.</span>
+        </p>
+      </div>
+      <div className="w-1/2 bg-primary flex items-center justify-center">
+        Right Half Content (e.g., Branding or Image)
+      </div>
+    </div>
+  );
 }
 
 export default Login;
