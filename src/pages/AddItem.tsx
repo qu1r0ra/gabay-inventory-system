c53fa5b15870158b64b49634c7d9c887e72381f2
Dawsxn <dawscatignas008@gmail.com>
2025-06-16 22:34:00 +0800

    feat(#6): create Add Item page

--- src/pages/AddItem.tsx
+++ src/pages/AddItem.tsx
@@ -1,9 +1,13 @@
 // ! Blank
 
+import AddItemForm from "../components/AddItemForm"
+
 function AddItem() {
     return (
         <>
-            <p>AddItem</p>
+            <div className="flex justify-center min-h-screen p-4 flex-col items-center">
+                <AddItemForm/>
+            </div>
         </>
     )
 }
