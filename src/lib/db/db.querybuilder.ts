3d63083fb225f52024de06f393e8a2567184c55d
jrgo7 <justinrainiergo@gmail.com>
2025-07-03 15:01:54 +0800

    feat: Add special handling for certain operators

--- src/lib/db/db.querybuilder.ts
+++ src/lib/db/db.querybuilder.ts
@@ -20,85 +20,52 @@ export function buildSelectQuery(
   let query = supabase.from(tableName).select();
 
   if (isStringRecord(params)) {
-    /**
-     *  Handle general filtering.
-     *  params["filters"] is expected to be an array of parameters to .filter()
-     *  by, following the ff. format:
-     *  filters: [
-     *    [column, operator, value],
-     *    ...
-     *    [columnN, operatorN, valueN]
-     *  ]
-     *  e.g.,
-     *  filters: [
-     *    ["status", "eq", "active"]
-     *    ["age", "gte", 123]
-     *  ]
-     * See {@link https://supabase.com/docs/reference/javascript/filter} for a
-     * list of supported filter operators.
-     */
-    if (Array.isArray(params["filters"])) {
-      for (const filter of params["filters"]) {
-        if (Array.isArray(filter)) {
-          const column = filter[0] as string;
-          const operator = filter[1] as string;
-          const value = filter[2];
-          query = query.filter(column, operator, value);
+    const operatorList = [
+      "eq", "neq", "gt", "gte", "lt", "lte", "like", "ilike", "is", "in",
+      "contains", "containedBy", "rangeGt", "rangeGte", "match", "overlaps",
+      "textSearch",
+
+      // Special handling required
+      "or", "not", "filter", "order"
+    ];
+
+    for (const operator of operatorList) {
+      const value = params[operator];
+      if ((operator === "or" || operator === "order") && value !== undefined) {
+        // 'or' expects raw PostgREST syntax, 'order' expects a column name
+        // ! This does not account for ordering options like ascending/descending (.reverse() can do that)
+        query = query.or(value as string);
+      } else if ((operator === "not" || operator === "filter") && value !== undefined) {
+        /**
+         *  'filter' and 'not' expect a list of lists of filters, e.g.
+         *   filter: [
+         *    ["status", "eq", "active"],
+         *    ["age", "gte", 123]
+         *  ]
+         *  */
+        if (Array.isArray(params[operator])) {
+          for (const filter of params[operator]) {
+            if (Array.isArray(filter)) {
+              const column = filter[0] as string;
+              const operator = filter[1] as string;
+              const value = filter[2];
+              // @ts-ignore: dynamic method call
+              query = query[operator](column, operator, value);
+            }
+          }
         }
-      }
-    }
-
-    /**
-     * Add more cases as needed. The above technically encompasses
-     * all that's needed, but it might be convenient to handle the more
-     * specific database query builder functions, too.
-     */
-    const operatorMap: Record<string, string> = {
-      eq: "eq",
-      neq: "neq",
-      gt: "gt",
-      gte: "gte",
-      lt: "lt",
-      lte: "lte",
-      like: "like",
-      ilike: "ilike", // case insensitive like
-      is: "is",
-      in: "in",
-      contains: "contains",
-      containedBy: "containedBy",
-      rangeGt: "rangeGt",
-      rangeGte: "rangeGte",
-      match: "match",
-      overlaps: "overlaps",
-    };
-
-    /**
-     * If your params object contains an operation
-     * 
-     * operation: {
-     *    column: value,
-     *    column2: value2,
-     * }
-     * 
-     * then it essentailly gets mapped into
-     * 
-     * query = query.operation(column, value).operation(column2, value2)
-     * 
-     * Note that some operations like `or`, `order`, `textSearch`, and `not`
-     * are handled differently, as they do not take a column-value pair
-     * 
-     */
-    for (const [paramKey, methodName] of Object.entries(operatorMap)) {
-      const value = params[paramKey];
-      if (isStringRecord(value)) { //
+      } else if (isStringRecord(value)) {
+        // General handling: query = query.operator(column, value[column]);
+        /**
+         * ! Supabase docs allow textSearch to be used with additional options,
+         * ! but this is not handled here
+         *  */
         for (const column of Object.keys(value)) {
           // @ts-ignore: dynamic method call
-          query = query[methodName](column, value[column]);
+          query = query[operator](column, value[column]);
         }
       }
     }
-
-
   }
 
   return query;
