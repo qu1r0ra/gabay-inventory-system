238ad97a28f3d47cd392e51f358857ae610ef75d
Dawsxn <dawscatignas008@gmail.com>
2025-07-21 03:37:56 +0800

    feat: create crud layout for mobile

--- src/pages/AddItem.tsx
+++ src/pages/AddItem.tsx
@@ -2,7 +2,7 @@ import ItemForm from "../components/ItemForm";
 
 function AddItem() {
   return (
-    <div className="flex justify-center min-h-screen flex-col items-center">
+    <div className="flex mt-8 mx-2 min-h-screen flex-col items-center">
       <ItemForm mode="add" />
     </div>
   );
